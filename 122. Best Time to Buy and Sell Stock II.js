64f6251f6d834225bf721ab670a4845e21897f06
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-10-31 15:04:38 -0700

    Complete 122. Best Time to Buy and Sell Stock II

---
+++ 122. Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,12 @@
+var maxProfit = function(prices) {
+    let profit = 0;
+    if (prices.length <= 1) {
+        return 0;
+    }
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i-1]) {
+            profit = profit + prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+};
