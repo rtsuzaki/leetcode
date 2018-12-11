6fcdba21de4fddcd55014662d59d91af8d887c98
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-12-10 15:36:24 -1000

    Complete 643. Maximum Average Subarray I

---
+++ 643. Maximum Average Subarray I.js
@@ -0,0 +1,14 @@
+var findMaxAverage = function(nums, k) {
+    //slideing window solution
+    let sum = 0;
+    let max = -Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (i >= k - 1) {
+            max = Math.max(max, sum / k);
+            sum -= nums[i - k + 1];
+        }
+    }
+
+    return max;
+};
