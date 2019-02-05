843fc008286ce7dfaf9b81e7f04e46bc4594b1b3
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-05 01:12:49 -0800

    Complete 206. Reverse Linked List

--- 92. Reverse Linked List II.js
+++ 92. Reverse Linked List II.js
@@ -24,3 +24,16 @@ var reverseBetween = function(head, m, n) {
     }
     return head;
 };
+
+var reverseList = function(head) {
+    if (head === null || head.next === null)   return head;
+    let prev = null;  // this will be new tail
+    let curr = head;
+    while(curr !== null) {
+        let nextTemp = curr.next;   // temp variable
+        curr.next = prev;   // redirect curr next
+        prev = curr;   // move prev down list (to right)
+        curr = nextTemp;   // move curr down list (to right)
+    }
+    return prev;   // will return list in reverse
+}; 
