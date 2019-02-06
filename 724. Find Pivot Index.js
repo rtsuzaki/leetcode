b3d752a998a835b4e951b4c30598bd1fa97a531f
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-06 14:19:51 -0800

    Fix 724. Find Pivot Index

--- 724. Find Pivot Index.js
+++ 724. Find Pivot Index.js
@@ -1,21 +1,20 @@
 var pivotIndex = function(nums) {
-    if (nums < 3) {
+    if (nums.length === 0) {
         return -1;
     }
     let total = nums.reduce((a,b) => a + b);
-    console.log(total);
-    let currentSum = 0;
+    console.log(total)
+    let currSum = 0;
+    
     for (let i = 0; i < nums.length; i++) {
-        if (i !== 0) {
-           currentSum += nums[i - 1]; 
+        if (i === 0) {
+            currSum = 0;
         } else {
-            currentSum = 0;
+            currSum += nums[i - 1]; 
         }
-        
-        console.log(currentSum, i)
-        if (currentSum === (total - nums[i])/2) {
+        if (currSum === ((total - nums[i])/2)) {
             return i;
         }
     }
-    return -1;
+    return - 1;
 };
