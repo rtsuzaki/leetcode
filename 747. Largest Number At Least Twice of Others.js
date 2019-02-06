7014a5f1223465b999fb1938c861d46f4efe18ab
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-06 14:28:32 -0800

    Redo 747. Largest Number At Least Twice of Others

--- 747. Largest Number At Least Twice of Others.js
+++ 747. Largest Number At Least Twice of Others.js
@@ -2,10 +2,10 @@ var dominantIndex = function(nums) {
     let max = Math.max(...nums);
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== max) {
-            if (2*nums[i] > max) {
-                return -1;
+            if (nums[i]*2 > max) {
+                return - 1;
             }
         }
     }
-    return nums.indexOf(max);
+    return nums.indexOf(max)
 };
