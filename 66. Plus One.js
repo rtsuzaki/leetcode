8430e0c7f396014fb2319e7dc85bb73c4aa8da72
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-06 14:42:39 -0800

    Redo 66. Plus One

--- 66. Plus One.js
+++ 66. Plus One.js
@@ -1,21 +1,19 @@
 var plusOne = function(digits) {
-  let output = null;
-  let add = (newDigits, changePlace) => {
-      if (newDigits[changePlace] !== 9) {
-          newDigits[changePlace]++;
-          output = newDigits;
-          return;
-      } else {
-          newDigits[changePlace] = 0;
-          if (changePlace === 0) {
-              newDigits.unshift(1);
-              output = newDigits;
-              return;
-          }
-          changePlace--;
-          add(newDigits, changePlace);
-      }
-  }
-  add(digits, digits.length-1);
-  return output
+    const add = (index) => {
+        if (digits[index] !== 9) {
+            digits[index] += 1;
+            return digits;
+        } else if (digits[index] === 9) {
+            if (index !== 0) {
+                digits[index] = 0;
+                console.log(digits)
+                return add(index - 1);    
+            } else {
+                digits[index] = 0;
+                digits.unshift(1);
+                return digits;
+            }
+        }
+    }
+    return add(digits.length - 1)
 };
