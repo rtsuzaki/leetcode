9620b353ccc9c74960f514a0e3160e76e1fe2e89
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-06 17:14:30 -0800

    Redo 14. Longest Common Prefix

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -1,21 +1,17 @@
 var longestCommonPrefix = function(strs) {
-    let output = '';
     if (strs.length === 0) {
-        return output
+        return '';
+    } else if (strs.length === 1) {
+        return strs[0];
     }
+    
     for (let i = 0; i < strs[0].length; i++) {
-        for (let j = 0; j < strs.length; j++) {
-            if (strs[0][i] !== strs[j][i]) {
-                return output
-            } else {
-                if (j === strs.length - 1) {
-                    output += strs[0][i]
-                    console.log(output)
-                }
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== strs[0][i]) {
+                return strs[0].slice(0, i);
             }
         }
     }
-    return output
+    
+    return strs[0];
 };
-
-longestCommonPrefix(["flower","flow","flight"])
