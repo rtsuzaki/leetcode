413716d7ec4588b826a92cfae27d22ae8200f9d1
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-07 15:43:03 -0800

    Redo 119. Pascal's Triangle II.js

--- 119. Pascal's Triangle II.js
+++ 119. Pascal's Triangle II.js
@@ -1,19 +1,24 @@
 var getRow = function(rowIndex) {
     let root = [1];
-    const getRow = (lastRow, count) => {
-        if (count === rowIndex) {
-            return lastRow;
+    if (rowIndex < 0) {
+        return [];
+    }
+    if (rowIndex === 0) {
+        return root;
+    }
+    const makeRow = (prevRow, prevLevel) => {
+        if (prevLevel === rowIndex) {
+            return prevRow;
         }
-        let row = [1];
-        for (let i = 0; i < lastRow.length; i++) {
-            if (i < lastRow.length - 1) {
-                row.push(lastRow[i] + lastRow[i + 1]);
-            } else {
-                row.push(1);
-            }
+        
+        let nextRow = [1];
+        for (let i = 0; i < prevRow.length - 1; i++) {
+            nextRow.push(prevRow[i] + prevRow[i + 1]);
         }
-        return getRow(row, count+1)
+        nextRow.push(1);
+        
+        return makeRow(nextRow, prevLevel + 1);
     }
     
-    return getRow(root, 0)
+    return makeRow(root, 0);
 };
