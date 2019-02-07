333b7e7b38f372a6072b48ede8380201acc7ab91
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-07 15:18:33 -0800

    Redo 27. Remove Element.js

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -1,9 +1,8 @@
 var removeElement = function(nums, val) {
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === val) {
-            nums.splice(i, 1);
+            nums.splice(i,1);
             i--;
         }
     }
-    console.log(nums);
-};
+}
