68f7b35d1b6d18f4bf4084c593281a543cce3c23
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2019-02-06 17:20:57 -0800

    Redo 561. Array Partition I

--- 561. Array Partition I.js
+++ 561. Array Partition I.js
@@ -1,9 +1,8 @@
 var arrayPairSum = function(nums) {
-    let sortedNums = nums.sort(function(a,b) { return a - b; })
-    console.log(sortedNums)
+    nums.sort((a, b) => a -b);
     let sum = 0;
-    for (let i = 0; i < sortedNums.length; i += 2) {
-        sum += Math.min(sortedNums[i], sortedNums[i+1]);
+    for (let i = 0; i < nums.length; i+=2) {
+        sum += nums[i];
     }
-    return sum
+    return sum;
 };
